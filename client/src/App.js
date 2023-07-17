7b368c035ce227890cc624ccb173cc7622576b66
Dev <devextraemail@gmail.com>
2023-07-17 16:59:41 -0400

    Tailwind init.

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="Pive">
       <header className="header">
-        <h1>
+        <h1 className='text-red-600 font-bold text-3xl mt-8 ml-8'>
           Pive.
         </h1>
       </header>
