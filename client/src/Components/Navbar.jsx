778712667edf0e03340bcc0b91356da4ad70f641
Dev <devextraemail@gmail.com>
2023-07-18 11:57:37 -0400

    Mobile/iPad design for navbar

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -2,15 +2,21 @@ import React from 'react';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
 
 const Navbar = () => {
+  const isSmallScreen = window.innerWidth <= 660; //
+
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50 flex items-center justify-between p-4 bg-blue-400 shadow-2xl">
-      <div className="flex items-center">
-        <h1 className="text-white text-5xl ml-8 py-4">Pive</h1>
+    <nav className="fixed top-0 left-0 right-0 z-50 flex flex-wrap items-center justify-between p-4 bg-blue-400 shadow-2xl">
+      <div className="flex items-center w-full sm:w-auto">
+        <h1
+            className={`text-white text-5xl ${isSmallScreen ? 'mx-auto' : 'ml-8'} py-4`}
+          >
+            Pive
+        </h1>
       </div>
-      <div className="relative flex items-center">
+      <div className="relative flex items-center w-full sm:w-auto mt-4 sm:mt-0">
         <input
           type="text"
-          className="w-96 pl-10 pr-4 py-2 rounded-lg bg-gray-200 focus:outline-none focus:ring focus:ring-blue-300"
+          className="w-full sm:w-96 pl-10 pr-4 py-2 rounded-lg bg-gray-200 focus:outline-none focus:ring focus:ring-blue-300"
           placeholder="Search beers, wine, liquors..."
         />
         <MagnifyingGlassIcon className="absolute w-5 h-5 text-gray-400 left-3" />
