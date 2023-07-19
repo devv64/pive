1cd2a5794b07e7fae1abeec3d413372061aeb912
arjunajesh <34989598+arjunajesh@users.noreply.github.com>
2023-07-19 16:36:13 -0400

    created schema

--- server/order/models.py
+++ server/order/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from product.models import Product
 
 # Create your models here.
+class Order(models.Model):
+    address = models.CharField(max_length=100)
+    contact = models.CharField(max_length=50)
+    status = models.CharField(max_length=10)
+    ordered_at = models.DateTimeField()
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
--- server/product/models.py
+++ server/product/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from stores.models import Store
 
 # Create your models here.
+class Drink(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+class Product(models.Model):
+    drink = models.ForeignKey(Drink, on_delete=models.CASCADE)
+    size = models.CharField(max_length=30)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    image_url = models.CharField(max_length=300)
+    stores = models.ManyToManyField(Store)
+    
