78ba96b9b727eb813233300e907ef5fc3e77e162
arjunajesh <34989598+arjunajesh@users.noreply.github.com>
2023-07-20 18:51:03 -0400

    mptt category implemented

--- server/product/models.py
+++ server/product/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 from stores.models import Store, StoreSerializer
 from rest_framework import serializers
+from mptt.models import MPTTModel, TreeForeignKey
 
 # Create your models here.
+class Category(MPTTModel):
+    name = models.CharField(max_length=15, unique=True)
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True,related_name='children')
+
 class Drink(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+    category = TreeForeignKey(Category, null=True, on_delete=models.SET_NULL)
 
 class Product(models.Model):
     drink = models.ForeignKey(Drink, on_delete=models.CASCADE)
@@ -18,6 +24,7 @@ class ProductStoreInfo(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     stock = models.IntegerField()
+
 
 class DrinkSerializer(serializers.ModelSerializer):
     class Meta:
