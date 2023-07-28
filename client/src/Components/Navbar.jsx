cb6523110accdc32f6564e95cdb89f176c92bd46
Dev <devextraemail@gmail.com>
2023-07-28 17:49:36 -0400

    quick commit

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
 import Cart from './Cart';
 import { Link } from 'react-router-dom';
