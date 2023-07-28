afaf09509798214f0d0fed8a5f33dfb6a81ad25d
Dev <devextraemail@gmail.com>
2023-07-28 17:46:59 -0400

    Connected google maps to landing page

--- client/src/Components/Landing.jsx
+++ client/src/Components/Landing.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useLoadScript, Autocomplete } from '@react-google-maps/api'
+const { MAPS_API_KEY } = require('./config')
 
 function Landing() {
+  const libraries = ["places"]
+  const {isLoaded, loadError} = useLoadScript({
+    googleMapsApiKey: MAPS_API_KEY,
+    libraries
+  })
+  if(loadError) return "Error loading maps";
+  if(!isLoaded) return "Loading Maps";
+
   return (
     <div>
         Landing
+        <Autocomplete>
+          <input 
+            id="autocomplete" 
+            placeholder="Enter delivery address" 
+            type="text"
+          />
+        </Autocomplete>
         <div>
             <Link to="/home">Home</Link>
         </div>
