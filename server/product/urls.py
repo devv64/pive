2505005cb3505d7ec159938b1543bf4fa2f6d505
arjunajesh <34989598+arjunajesh@users.noreply.github.com>
2023-07-29 15:22:53 -0400

    expanded data set + product list from query view

--- server/product/urls.py
+++ server/product/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("featured_carousel", views.get_carousel_featured_drinks),
     path("category/<str:category>", views.get_carousel_drinks_by_category),
     path("search/<str:query>", views.get_autocomplete_results),
+    path("search_results/<str:query>/<int:page_num>", views.get_query_results),
 
     path("create", views.create),
 ]
